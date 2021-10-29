70c368329c8fdba8ab08921b07a66b8230ea3048
jm <rjm9303@gmail.com>
2021-10-29 17:30:46 +0900

    vue-news completed

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,27 +1,11 @@
-export default {
-  fetchedItem(state) {
-    return state.item;
+import { RootState } from "@/store/state";
+
+const getters = {
+  fetchedNews(state: RootState) {
+    return state.news;
   },
-  fetchedUser(state) {
-    return state.user;
-  },
-  fetchedList(state) {
-    return state.list;
-  },
-  // ItemView
-  userName(state) {
-    return state.item.user;
-  },
-  userContent(state) {
-    return state.item.content;
-  },
-  userQuestion(state) {
-    return state.item.title;
-  },
-  userTimeAgo(state) {
-    return state.item.time_ago;
-  },
-  contentPoints(state) {
-    return state.item.points;
-  },
-}
+};
+
+type Getters = typeof getters;
+
+export { getters, Getters };
--- src/store/types.ts
+++ src/store/types.ts
@@ -2,6 +2,7 @@ import { CommitOptions, DispatchOptions, Store } from "vuex";
 import { Mutations } from "./mutations";
 import { RootState } from "./state";
 import { Actions } from "@/store/actions";
+import { Getters } from "@/store/getters";
 
 type MyMutations = {
   commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
@@ -19,7 +20,17 @@ type MyActions = {
   ): ReturnType<Actions[K]>;
 };
 
+type MyGetters = {
+  getters: {
+    [K in keyof Getters]: ReturnType<Getters[K]>;
+  };
+};
+
 // 이름은 앱에 맞게 커스텀
-export type MyStore = Omit<Store<RootState>, "commit" | "dispatch"> &
+export type MyStore = Omit<
+  Store<RootState>,
+  "commit" | "dispatch" | "getters"
+> &
   MyMutations &
-  MyActions;
+  MyActions &
+  MyGetters;
